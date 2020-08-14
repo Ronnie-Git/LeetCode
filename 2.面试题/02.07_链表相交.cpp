e6760aab0fc97a4e33fa474049441bb24d4ccec0
Ronnie-Git <441294402@qq.com>
2020-08-14 10:48:07 +0800

    add 02.07

---
+++ 2.面试题/02.07_链表相交.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 02.07_链表相交.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 14 Aug 2020 10:47:19 AM CST
+ ************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p = headA, *q = headB;
+        while (p != q) {
+            p = p ? p->next : headB;
+            q = q ? q->next : headA;
+        }
+        return p;
+    }
+};
